f414d961e0387fc21c8344d505b4949c0ced5841
asad624465 <mdasadurjjaman@gmail.com>
2022-08-29 01:36:29 +0600

    mastering frontend design

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'account',
+    'store',
 ]
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 MIDDLEWARE = [
@@ -44,7 +50,7 @@ ROOT_URLCONF = 'Ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,12 +111,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+"""STATIC_URL = 'static/'
 STATICTILES_DIRS = [os.path.join(BASE_DIR,'static'),]
-STATIC_ROOT=os.path.join(BASE_DIR,'assets/')
+STATIC_ROOT=os.path.join(BASE_DIR,'static/assets')
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-
+"""
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR]
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
