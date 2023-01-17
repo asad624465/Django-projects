d2c25336be4be42c060aada7749c6d9e96e3f16f
asad624465 <mdasadurjjaman@gmail.com>
2023-01-18 00:01:13 +0600

    added search system

--- store/views.py
+++ store/views.py
@@ -1,19 +1,24 @@
 from multiprocessing import context
 from django.shortcuts import render
 
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,TemplateView
 #load model for showing product in home page
 from store.models import Product,Category,ProductImage,Banner
 
-class ProductsList(ListView):
-    model= Product
-    template_name='frontend/index.html'
-    context_object_name='products'
+class ProductsList(TemplateView):
+    def get(self,request,*args, **kwargs):
+        products=Product.objects.all().order_by('-id')
+        banners=Banner.objects.filter(is_active=True).order_by('-id')[0:3]
+        context={'products':products,'banners':banners}
+        return render(request,'frontend/index.html',context);
 
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args,**kwargs)
-        context["banners"] = Banner.objects.filter(is_active=True).order_by('-id')[0:3]
-        return context
+    def post(self,request,*args, **kwargs):
+        if request.method=="POST" or request.method=="post":
+            name=request.POST.get('item')
+            products=Product.objects.filter(name__icontains=name).order_by('-id')
+            context={'products':products,'banners':''}
+            return render(request,'frontend/index.html',context);
+            
     
 class productDetails(DetailView):
     model=Product
